2e3b29fa619400a17f6495919d16fd04a9bbc6d4
AdonayMejia <adonay.mejia021@gmail.com>
2023-06-01 14:34:54 -0600

    feat: Adding Ktor dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation(libs.coil.compose)
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.navigation.compose)
+    implementation(libs.bundles.ktor)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
